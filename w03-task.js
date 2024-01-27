0878681f40a214fd325ececb5f6ba4733a9bf93c
vladmirnzeh <vladmirkenneth@gmail.com>
2024-01-27 16:35:59 +0100

    Second Modification

--- w03-task.js
+++ w03-task.js
@@ -10,16 +10,16 @@ function add(number1, number2) {
   
   // Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of add1 and add2. Pass them to the add function
   function addNumbers() {
-    let add1 = parseInt(document.querySelector('#add1').value);
-    let add2 = parseInt(document.querySelector('#add2').value);
+    let add1 = parseInt(document.getElementById('#add1').value);
+    let add2 = parseInt(document.getElementById('#add2').value);
     let result = add(add1, add2);
   
     // Step 4: Assign the return value to an HTML form element with an ID of sum
-    document.querySelector('#sum').value = result;
+    document.getElementById('#sum').value = result;
   }
   
   // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
-  document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+  document.getElementById('#addNumbers').addEventListener('click', addNumbers);
   
   // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
   const subtract = function (number1, number2) {
@@ -27,28 +27,28 @@ function add(number1, number2) {
   };
   
   const subtractNumbers = function () {
-    let subtract1 = parseInt(document.querySelector('#subtract1').value);
-    let subtract2 = parseInt(document.querySelector('#subtract2').value);
+    let subtract1 = parseInt(document.getElementById('#subtract1').value);
+    let subtract2 = parseInt(document.getElementById('#subtract2').value);
     let result = subtract(subtract1, subtract2);
-    document.querySelector('#difference').value = result;
+    document.getElementById('#difference').value = result;
   };
   
   document
-    .querySelector('#subtractNumbers')
+    .getElementById('#subtractNumbers')
     .addEventListener('click', subtractNumbers);
   
   // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and multiplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
   const multiply = (number1, number2) => number1 * number2;
   
   const multiplyNumbers = () => {
-    let factor1 = parseInt(document.querySelector('#factor1').value);
-    let factor2 = parseInt(document.querySelector('#factor2').value);
+    let factor1 = parseInt(document.getElementById('#factor1').value);
+    let factor2 = parseInt(document.getElementById('#factor2').value);
     let result = multiply(factor1, factor2);
-    document.querySelector('#product').value = result;
+    document.getElementById('#product').value = result;
   };
   
   document
-    .querySelector('#multiplyNumbers')
+    .getElementById('#multiplyNumbers')
     .addEventListener('click', multiplyNumbers);
   
   // Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
@@ -57,20 +57,31 @@ function add(number1, number2) {
   }
   
   function divideNumbers() {
-    let dividend = parseFloat(document.querySelector('#dividend').value);
-    let divisor = parseFloat(document.querySelector('#divisor').value);
+    let dividend = parseFloat(document.getElementById('#dividend').value);
+    let divisor = parseFloat(document.getElementById('#divisor').value);
     let result = divide(dividend, divisor);
-    document.querySelector('#quotient').value = result;
+    document.getElementById('#quotient').value = result;
   }
   
   document
-    .querySelector('#divideNumbers')
+    .getElementById('#divideNumbers')
     .addEventListener('click', divideNumbers);
   
-  // Step 9: Test all of the mathematical functionality of the task3.html page.
   
-  /* BUILT-IN METHODS */
-  
+  /* Decision Makers */
+
+  function getTotal(){
+    //Input
+    let subtotal = parseFloat(document.getElementById("subtotal").value);
+    //Processing
+    if (document.getElementById("member").checked){
+        subtotal = subtotal - subtotal * 0.2; //20% discount
+    }
+    //Output
+    document.getElementById("total").textContent = `$${subtotal.toFixed(2)}`;
+  }
+  document.getElementById('getTotal').addEventListener('click', getTotal);
+
   // Step 1: Declare and instantiate a variable of type Date to hold the current date
   const currentDate = new Date();
   
@@ -85,37 +96,36 @@ function add(number1, number2) {
   
   /* ARRAY METHODS */
   
-  // Step 1: Declare and instantiate an array variable to hold the numbers 1 through 25
+  // Step 1: Declare and instantiate an array variable to hold the numbers 1 through 13
   const numbers = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-    23, 24, 25
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
   ];
   
   // Step 2: Assign the value of the array variable to the HTML element with an ID of "array"
-  document.querySelector("#array").textContent = numbers;
+  document.getElementById("#array").textContent = numbers;
   
   // Step 3: Use the filter array method to find all of the odd numbers of the array variable and assign the reult to the HTML element with an ID of "odds" ( hint: % (modulus operartor) )
-  document.querySelector("#odds").textContent = numbers.filter(
+  document.getElementById("#odds").textContent = numbers.filter(
     (number) => number % 2
   );
   
   // Step 4: Use the filter array method to find all of the even numbers of the array variable and assign the result to the HTML element with an ID of "evens"
-  document.querySelector("#evens").textContent = numbers.filter(
+  document.getElementById("#evens").textContent = numbers.filter(
     (number) => number % 2 === 0
   );
   
   // Step 5: Use the reduce array method to sum the array variable elements and assign the result to the HTML element with an ID of "sumOfArray"
-  document.querySelector("#sumOfArray").textContent = numbers.reduce(
+  document.getElementById("#sumOfArray").textContent = numbers.reduce(
     (sum, number) => sum + number,
     0
   );
   
   // Step 6: Use the map array method to multiply each element in the array variable by 2 and assign the result to the HTML element with an ID of "multiplied"
-  document.querySelector("#multiplied").textContent = numbers.map(
+  document.getElementById("#multiplied").textContent = numbers.map(
     (number) => number * 2
   );
   
   // Step 7: Use the map and reduce array methods to sum the array elements after multiplying each element by two.  Assign the result to the HTML element with an ID of "sumOfMultiplied"
-  document.querySelector("#sumOfMultiplied").textContent = numbers
+  document.getElementById("#sumOfMultiplied").textContent = numbers
     .map((number) => number * 2)
     .reduce((sum, number) => sum + number, 0);
