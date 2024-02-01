40abd382a321b71641ad814cb11f1e20a4d43d17
vladmirnzeh <vladmirkenneth@gmail.com>
2024-02-01 11:28:05 +0100

    w04 task mod

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,53 +4,26 @@
 
 /* Step 2 - Variables */
 
-// declare and instantiate a variable to hold your name
-const myName = "Emmanuel Chidiebere Nzeh"
 
-// place the value of the name variable into the HTML file (hint: document.querySelector())
-document.querySelector("#name").innerHTML = myName
 
-// declare and instantiate a variable to hold the current year
-const year = 2022
 
-// place the value of the current year variable into the HTML file
-document.querySelector("#year").innerHTML = year
+/* Step 3 - Element Variables */
 
-// declare and instantiate a variable to hold the name of your picture
-const picture = "images/Ashsuit.jpg"
 
-// copy your image into the "images" folder
 
-// place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-document.querySelector("img").setAttribute("src", picture)
+
+
+/* Step 4 - Adding Content */
+
+
+
 
 
 
 /* Step 5 - Array */
 
-// declare and instantiate an array variable to hold your favorite foods
-let tastyFoods = ["Beans", "Steak", "Potatoes", "Plantains", "Moi Moi", "Pineapple", "Apple", "Banana"]
 
-// place the values of the favorite foods variable into the HTML file
-document.querySelector("#food").innerHTML = tastyFoods
 
-// declare and instantiate a variable to hold another favorite food
-const otherFood = "Milkshakes"
 
-// add the variable holding another favorite food to the favorite food array
-tastyFoods.push(otherFood)
 
-// repeat 
-document.querySelector("#food").innerHTML = tastyFoods
 
-// remove the first element in the favorite foods array
-tastyFoods.shift()
-
-// repeat 
-document.querySelector("#food").innerHTML = tastyFoods
-
-// remove the last element in the favorite foods array
-tastyFoods.pop()
-
-// repeat 
-document.querySelector("#food").innerHTML = tastyFoods
---
+++ scripts/w04-task.js
@@ -0,0 +1,62 @@
+/* Profile Object  */
+
+let myProfile = {
+    name: "Emmanuel Chidiebere Nzeh",
+    photo: "images/Ashsuit.jpg",
+    favoriteFoods: [
+        "Beans", "Plantain", "Potatoes", "Soya Beans", "Bean Cake", "Bannana", "Yoghurt", "Chocolate", "Egusi"
+    ],
+    hobbies: [
+        "Cooking", "Walking", "Coding", "Playing video games", "Travelling", "Reading", "Knacking"
+    ],
+    placesLived: [
+        {
+            place: "Lagos, Nigeria",
+            length: "23 years"
+        },
+        {
+            place: "Accra, Ghana",
+            length: "2 weeks"
+        },
+        {
+            place: "Cape Town, South Africa",
+            length: "2 years"
+        },
+        {
+            place: "Nairobi, Kenya",
+            length: "2 years"
+        }
+    ]
+};
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
+/* Photo with attributes */
+document.querySelector('#photo').src = myProfile.photo;
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let listItem = document.createElement('li');
+    listItem.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(listItem);
+})
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let listItem = document.createElement('li');
+    listItem.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(listItem);
+})
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dtElement = document.createElement('dt');
+    dtElement.textContent = place.place;
+
+    let ddElement = document.createElement('dd');
+    ddElement.textContent = place.length;
+
+    document.querySelector('#places-lived').appendChild(dtElement);
+    document.querySelector('#places-lived').appendChild(ddElement);
+
+})
